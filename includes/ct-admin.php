08dc4815afa0a343d21f233cff2cbacfdc6c2b8c
think201 <think201@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-31 14:28:11 +0000

    Few Issues Fixes.
    git-svn-id: https://plugins.svn.wordpress.org/clients/trunk@1079583 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/ct-admin.php
+++ includes/ct-admin.php
@@ -67,7 +67,7 @@ class CTAdmin
     {
         if (!is_admin()) 
         {           
-            wp_enqueue_style( 'ct-css', plugins_url( 'clients/assets/css/ct.css' ), array(), FR_VERSION, 'all' );
+            wp_enqueue_style( 'ct-css', plugins_url( 'clients/assets/css/ct.css' ), array(), CT_VERSION, 'all' );
         }
     }    
 
